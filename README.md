e0ec9d7696ae694c11d7356d3d9c7d984576fb0d
Pedro de Carli <pddecarli@hotmail.com>
2021-07-10 21:03:49 -0400

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,6 @@
+## Project template
+This repository is a simple next app template to use while initializing a new next.js project.
+### To use this template just run the following command:
+```bash
+npx create-next-app name-of-the-new-project --example https://github.com/pedro7021/next-example
+```
---
+++ src/styles/theme.ts
@@ -0,0 +1,43 @@
+export const theme = {
+  colors: {
+    primary: '#000000',
+    darkText: '#333333',
+    secondary: '#dc143c',
+    white: '#FFFFFF',
+    mediumGray: '#DDDDDD',
+    darkerGray: '#BBBBBB',
+  },
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+    },
+    sizes: {
+      xsmall: '0.8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+    lteSmall: '(max-width: 360px)',
+  },
+  spacings: {
+    xsmall: '0.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
+  sizes: {
+    max: '96rem',
+    content: '80rem',
+  },
+} as const;
